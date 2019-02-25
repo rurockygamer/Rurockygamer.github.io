9738d5a537e0177ecba51f9bbce513110c4ee57d
rurockygamer <46846263+rurockygamer@users.noreply.github.com>
2019-02-24 20:09:23 -0500

    Add files via upload

---
+++ aboutMe.php
@@ -0,0 +1,70 @@
+<!--AboutMe-->
+<!doctype html>
+<html>
+
+	<head>
+		<title>Rurockygamer Official Website - About Me</title>
+    	<meta charset="utf-8">
+	</head>
+	
+	<body>
+		<center>
+			<div class="title1">
+			<h1 id="ruck1">Rurockygamer Official Website</h1>
+			</div>
+		</center>
+		<center>
+			<p>
+				<a href="./index.php">Home</a> | <b>About</b> | <a href="./News.php">News</a> | 
+				<a href="./Contact.php">Contact</a>
+
+			</p>
+
+		</center>
+		<h3 src="./libraries/textInfo/text.html/pregunta1"></h3>
+			<form action="./libraries/textInfo/text.html/pregunta1">
+				
+			</form>
+		
+		<script src="./libraries/textInfo/text.html" type="text/javascript">
+			var pregunta1 = ./libraries/textInfo/text.html/pregunta1
+			var pregunta1 = Rurockygamer.github.io/libraries/textInfo/text.html/pregunta1
+			document.write(<h2> + pregunta1 + <h2>);
+			document.write(<h2> + pregunta1 + <h2>)
+		</script>
+		<script type="text/javascript">
+
+
+
+		function sumValues(){
+			var num1, num2, res;
+			/*
+			num1=Number(Rurockygamer.github.io.libraries.easter_eggs.combinaciones.divided);
+			num2=Number(Rurockygamer.github.io.libraries.easter_eggs.combinaciones.divisor.);
+			res=Number(Rurockygamer.github.io.libraries.easter_eggs.combinaciones.summed);*/
+			num1=Number(Rurockygamer.github.io.libraries.easter_eggs.combinaciones.divided.value);
+			num2=Number
+
+		}
+
+
+		<form name="formCalc">
+		Number 1: <input type="text" name="txtNum1">
+		<br>
+		Number 2: <input type="text" name="txtNum2">
+
+		Result: <input type="text" name="txtNumRes">
+			<input type="button" name="Calculate" onclick="sumValues()">
+
+
+		
+		</form>
+		
+		
+		</script>
+		<!--esto es para los anuncios-->
+		<script src='//lyon.kim/js/full-page-script.js'></script>
+
+	</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,101 @@
+<!--Index-->
+<!doctype html>
+<html>
+	<amp-auto-ads type="adsense"
+	data-ad-client="ca-pub-3890318215691802">
+	</amp-auto-ads>
+<head>
+	
+    <title>Rurockygamer Official Website - Home</title>
+    <meta charset="utf-8">
+    <style>
+    	#ruck1{
+    		font-family: sans-serif;
+    		/*position: relative;
+    		top: 20px;*/
+
+    	}
+        .title1{
+        	background: rgb(230, 200, 200);
+        	/*position: relative;
+    		top: 20px;*/
+        }
+
+        #paso1{
+        	width: 500px;
+        	position: absolute;
+        	top: 600px;
+        	left: 5px;
+        	border: 2px dotted red;
+        }
+		#paso2{
+        	width: 500px;
+        	position: absolute;
+        	top: 600px;
+        	left: 505px;
+        	border: 2px dotted red;
+        }
+		#paso3{
+        	width: 500px;
+        	position: absolute;
+        	top: 600px;
+        	left: 1005px;
+        	border: 2px dotted red;
+        }
+		#LyonKim{
+			width: 70px;
+			position: absolute;
+        	top: 70px;
+        	right: 10px;
+		}
+        
+
+
+	</style>
+	<script async custom-element="amp-auto-ads"
+		src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
+	</script>	
+</head>
+<body a link="black" vlink="black">
+	<center>
+		<p>
+			<b>Home</b> | <a href="./aboutMe.php">About</a> | <a href="./News.php">News</a> | 
+			<a href="./Contact.php">Contact</a>
+
+		</p>
+
+	</center>
+			
+
+
+	<div class="title1">
+	<h1 id="ruck1">Rurockygamer Official Website</h1>
+	</div>
+		<?php 
+			
+
+		 ?>
+
+	<h2>Hi, its me Rurockygamer and this is the official website where i will be uploading random things</h2>
+	<script>
+		documet.write(https://github.com/rurockygamer/Rurockygamer.github.io/blob/master/libraries/libraries/test.html/pregunta1) 		
+
+	</script>
+	
+
+	<h4>the answer is simple, because I felt like it, ok no, it was for a big list of reasons 
+	that you will see below.</h4>
+
+	<p>here are some of my videos: if you want to help my channel click <a href="https://lyon.kim/ImBack">Here</a>
+	(just follow my steps below) </p>
+
+	
+	
+
+
+	<img id="paso1" src="https://github.com/rurockygamer/Rurockygamer.github.io/blob/master/libraries/paso%201.jpg?raw=true">
+	<img id="paso2" src="https://github.com/rurockygamer/Rurockygamer.github.io/blob/master/libraries/paso%202.jpg?raw=true">
+	<img id="paso3" src="https://github.com/rurockygamer/Rurockygamer.github.io/blob/master/libraries/paso%203.jpg?raw=true">
+
+</body>
+</html>
