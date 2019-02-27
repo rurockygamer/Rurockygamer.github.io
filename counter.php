9409c5e716a512881b2180f6182c3da577132189
rurockygamer <46846263+rurockygamer@users.noreply.github.com>
2019-02-27 14:03:57 -0500

    Update counter.php

--- counter.php
+++ counter.php
@@ -26,7 +26,7 @@
 
   $counterVal = str_pad($counterVal, 5, "0", STR_PAD_LEFT);
   $chars = preg_split('//', $counterVal);
-  $im = imagecreatefrompng("canvas.png");
+  $im = imagecreatefrompng("Canvas.png");
 
   $src1 = imagecreatefrompng("$chars[1].png");
   $src2 = imagecreatefrompng("$chars[2].png");
