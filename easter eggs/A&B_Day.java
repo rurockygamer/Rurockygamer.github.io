4c281ad1f75f55fbf13bc0df4ad7310b3e286bf2
Ruben Hadjes <rhadjesr@gmail.com>
2021-02-06 16:35:19 -0500

    Update A&B_Day.java

--- easter eggs/A&B_Day.java
+++ easter eggs/A&B_Day.java
@@ -1,38 +1,61 @@
 public class A&B_Day
 {
 
-
+	//this is the date that you want to find whether if is A or B day
 	private int year;
 	private int month;
 	private int day;
 
-	for(y = 2020; y <= year; y++)
+	//this is the date of a day that is known to be A day
+	private static int AYear;
+	private static int AMonth;
+	private static int ADay;
+
+	public static int AorBDayFromDate(int year, int month, int day)
 	{
-		for(m = 1; m <= month; m++)
+
+		for(y = 2020; y <= year; y++)
 		{
-			if(m == 2){
+			for(m = 1; m <= month; m++)
+			{
+				if(m == 2){
 
-				for(d = 1; d <= 29; d++)
-				{
+					for(d = 1; d <= 29; d++)
+					{
 
+					}
+				} else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
+				
+					for(d = 1; d <= 31; d++)
+					{
+
+					}
+				} else {
+
+					for(d = 1; d <= 30; d++)
+					{
+
+					}
 				}
-			} else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 9 || m == 11) {
+
+			} //months	
+
+		} //years
+	}
+
+	/**this one does this: if yesterday was A day, today is B day
+	*/
+	public static int AorB(int dayBefore)
+	{
+		if (/*this will chech if today is not an A or B day*/){
 			
-				for(d = 1; d <= 30; d++)
-				{
+		} else if(dayBefore == 1){
+			return 0;
+		} else if (dayBefore == 0) {
+			return 1;
+		}
+	}
 
-				}
-			} else {
-
-				for(d = 1; d <= 31; d++)
-				{
-
-				}
-			}
-
-		} //months	
-
-	} //years
 
 }//the class
 
