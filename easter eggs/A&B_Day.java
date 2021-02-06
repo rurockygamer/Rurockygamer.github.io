525c0862b1b36cbb5dad1ac82fb9dd7d38b3c7c8
Ruben Hadjes <rhadjesr@gmail.com>
2021-02-06 08:29:50 -0500

    Create A&B_Day.java

---
+++ easter eggs/A&B_Day.java
@@ -0,0 +1,38 @@
+public class A&B_Day
+{
+
+
+	private int year;
+	private int month;
+	private int day;
+
+	for(y = 2020; y <= year; y++)
+	{
+		for(m = 1; m <= month; m++)
+		{
+			if(m == 2){
+
+				for(d = 1; d <= 29; d++)
+				{
+
+				}
+			} else if (m == 1 || m == 3 || m == 5 || m == 7 || m == 9 || m == 11) {
+			
+				for(d = 1; d <= 30; d++)
+				{
+
+				}
+			} else {
+
+				for(d = 1; d <= 31; d++)
+				{
+
+				}
+			}
+
+		} //months	
+
+	} //years
+
+}//the class
+
